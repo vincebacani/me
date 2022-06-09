53933bfe6318996c44806dcc51ecca98defafbe8
Vince Bacani <philipvincebacanii@gmail.com>
2022-06-09 17:20:47 +1000

    exercise0 done for real

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -79,7 +79,7 @@ def really_shout(a_string):
     that you've reused the function you already wrote.
     Look up how to 'concatinate' strings to make this happen.
     """
-    the_answer = None
+    the_answer = a_string.upper + "!"
     return the_answer
 
 
